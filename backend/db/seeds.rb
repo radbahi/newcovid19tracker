81875245279c55e68849b27dffe3c8236afcb460
teapuddles <kingsley.gleeson@gmail.com>
2021-01-19 15:25:03 -0500

    small seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+l1 = Location.create(country: "poopville", flag: "neato", ISO: "TSM", confirmed: 1, deaths: 10, active: 12, recovered: 0, lat: 1.999, lon: 29.3999, date: "Today")
+
+u1 = User.create(username: "poop", password_digest: "poop", locations_id: l1.id)
+
