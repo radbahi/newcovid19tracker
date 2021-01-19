e37d24f820a16f5422e36fb1723dded5810b1f48
Radouane Bahi <radbahi@gmail.com>
2021-01-19 14:59:13 -0500

    database initialized and schema created

---
+++ backend/db/migrate/20210119195246_create_locations.rb
@@ -0,0 +1,18 @@
+class CreateLocations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :locations do |t|
+      t.string :country
+      t.string :flag
+      t.string :ISO
+      t.integer :confirmed
+      t.integer :deaths
+      t.integer :active
+      t.integer :recovered
+      t.float :lat
+      t.float :lon
+      t.string :date
+
+      t.timestamps
+    end
+  end
+end
