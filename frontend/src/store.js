8166f123dacc89adc306ca763a9bee0f91075938
teapuddles <kingsley.gleeson@gmail.com>
2021-01-21 16:23:33 -0500

    store created, packages installed

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
+const initialState = { 
+    userLogin: { userInfo: userInfoFromStorage }
+}
+
+const middleware = [thunk]
+
+const reducer = combineReducers({})
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store 
