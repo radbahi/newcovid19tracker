d232cc4ba46094d90974eb1c911620ec096c76e9
teapuddles <kingsley.gleeson@gmail.com>
2021-03-03 12:56:52 -0500

    daily push

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -4,6 +4,56 @@ import { login } from '../actions/userActions.js'
 import { Button } from 'react-bootstrap'
 import styled from 'styled-components'
 
+
+const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: black;
+font-weight: bold;
+`
+
+const Form = styled.form`
+font-size: 1em;
+text-align: center;
+color: white;
+background-color: #317cf7;
+`
+
+const RegLink = styled.h3`
+font-size: 1.5em;
+text-align: center;
+color: white;
+background-color: #317cf7;
+font-weight: bold;
+`
+
+const LoginDiv = styled.div`
+border-radius: 25px;
+border: 2px solid;
+width: 300px;
+position: center;
+justify-content: center;
+box-shadow: 0px 0px 5px 7px #CED1DB;
+`
+
+const LoginWrapper = styled.div`
+margin: 200px;
+margin-left: 130px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+border-style: line;
+border-color: black;
+`
+
+// button currently uses bootstrap
+// const RegButton = styled.button`
+// color: palevioletred;
+// background-color: purple;
+// `
+
+
 const LoginScreen = ({ loginError }) => {
   const dispatch = useDispatch()
 
@@ -21,39 +71,8 @@ const LoginScreen = ({ loginError }) => {
   // const userState = useSelector((state) => state.userState)
 
   // const { userInfo } = userState
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-    background-color: purple`
-
-  const Form = styled.form`
-    font-size: 1em;
-    text-align: center;
-    color: palevioletred;
-    background-color: purple`
-  
-  const RegLink = styled.h3`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-  background-color: purple`
-
-  const LoginDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  width: 300px;
-  position: center;
-  `
-
-  // button currently uses bootstrap
-  const RegButton = styled.button`
-  color: palevioletred;
-  background-color: purple;
-  `
-  
   return (
+    <LoginWrapper>
     <LoginDiv>
       {loginError && loginError}
       <form onSubmit={submitLoginHandler}>
@@ -77,6 +96,7 @@ const LoginScreen = ({ loginError }) => {
       <RegLink>Don't have an account? Click below</RegLink>
       <Button href='/register'>Register a new account</Button>
     </LoginDiv>
+    </LoginWrapper>
   )
 }
 
