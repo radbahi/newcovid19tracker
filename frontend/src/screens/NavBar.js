dec73b5647ecdbcb171d9c1953774bbf9e08da89
teapuddles <kingsley.gleeson@gmail.com>
2021-03-03 23:15:31 -0500

    fixed typo now login + register work on navbar

--- frontend/src/screens/NavBar.js
+++ frontend/src/screens/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
             <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="app-links">
                 <Nav.Link href="/login"> Login </Nav.Link>
-                <Nav.Link href="/regsiter"> Register </Nav.Link>
+                <Nav.Link href="/register"> Register </Nav.Link>
                 <Nav.Link href="/about"> About </Nav.Link>
             </Nav>
             </Navbar.Collapse>
